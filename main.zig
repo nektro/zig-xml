8af81fbc072750c91da0be4951ebf4acdb511294
Meghan Denny <hello@nektro.net>
2026-04-26 16:50:39 -0700

    test: migrate to zig-time

--- main.zig
+++ main.zig
@@ -1,13 +1,14 @@
 const std = @import("std");
 const string = []const u8;
 const xml = @import("xml");
+const time = @import("time");
 
 pub fn main() !void {
     const path = std.mem.sliceTo(std.os.argv[1], 0);
     var file = try std.fs.cwd().openFile(path, .{});
     defer file.close();
-    var timer = try std.time.Timer.start();
+    const start = time.nanoTimestamp();
     var doc = try xml.parse(std.heap.c_allocator, path, file.reader());
     defer doc.deinit();
-    std.log.warn("{d}ms", .{timer.read() / std.time.ns_per_ms});
+    std.log.warn("{d}ms", .{(time.nanoTimestamp() - start) / time.ns_per_ms});
 }
